ee7f906bcdcf0abb75677f1f26281058eaecfec1
YOUR_USERNAMESong <boda2@illinois.edu>
2019-12-04 22:01:31 -0600

    0.8 Pokemon added

--- app/src/main/java/com/example/pokeapp/MainActivity.java
+++ app/src/main/java/com/example/pokeapp/MainActivity.java
@@ -28,6 +28,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final EditText searchBar = findViewById(R.id.searchBar);
         Button berrySearch = findViewById(R.id.berrySearch);
+        final Button pokeSearch = findViewById(R.id.pokemonSearch);
+        pokeSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputStr = searchBar.getText().toString();
+                if (inputStr.length() == 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Search bar can not be empty!", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    try {
+                        int input = Integer.parseInt(inputStr);
+                        if (input > 807 || input < 1) {
+                            AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                            alertDialog.setTitle("POKEMON DOES NOT EXIST");
+                            alertDialog.setMessage("The given index does not exist! Please enter a valid index.");
+                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                            alertDialog.show();
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, pokemonSearch.class);
+                            intent.putExtra("input", input);
+                            startActivity(intent);
+                        }
+                    } catch (Exception e) {
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                        alertDialog.setTitle("NOT AN INDEX");
+                        alertDialog.setMessage("Please enter an index!");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                        alertDialog.show();
+                    }
+                }
+            }
+        });
         berrySearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     try {
                         int input = Integer.parseInt(inputStr);
-                        if (input > 64) {
+                        if (input > 64 || input < 1) {
                             AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
                             alertDialog.setTitle("BERRY DOES NOT EXIST");
                             alertDialog.setMessage("The given index does not exist! Please enter a valid index.");
